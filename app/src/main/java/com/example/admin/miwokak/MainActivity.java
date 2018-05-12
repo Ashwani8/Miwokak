6998e772173fcc534f7230778b74ca5d9b11ab77
Ashwani Kumar <akumar@monmouthcollege.edu>
2018-05-12 12:49:49 -0500

    Modify AndroidManifest to display activity name and aded onClick listener using android xml

--- app/src/main/java/com/example/admin/miwokak/MainActivity.java
+++ app/src/main/java/com/example/admin/miwokak/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.admin.miwokak;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
     }
+
+        public void openNumbersList(View view){
+            Intent i = new Intent(this, NumbersActivity.class);
+            startActivity(i);
+    }
+
 }
